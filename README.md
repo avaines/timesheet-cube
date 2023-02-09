9447201ff13b3a4dd84c493861ef8736326999b8
n3rden <aiden@vaines.org>
2023-02-09 20:43:56 +0000

    Initial commit, working 365

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # timesheet-cube
 A janky Timeular alternative using an Aqara cube and an Office365 calendar
+
+I am not paying £70 for a bit of plastic and £10 a month to use it. But I would find some of the features. namely, a way of tracking which project I'm currently working on and for it to be historically referable.
+
+I already have an Office 365 subscription with calendars and I don't mind spending £20 on AliExpress,
+
+
+## Shopping list
+1) An Aqara Cube: https://www.aliexpress.com/af/aqara-cube.html
+2) A USB Zigbee gateway: https://www.aliexpress.com/af/usb-zigbee.html
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import time
+import pytz
 
 from datetime import datetime, timedelta
-from dotenv import load_dotenv
 from O365 import Account
+from progress.bar import IncrementalBar
 
+
+from dotenv import load_dotenv
+load_dotenv()
 
 def o365_auth():
     credentials = (
@@ -27,7 +31,7 @@ def o365_auth():
         print('Authenticated')
         return account
 
-def new_calendar_event(calendar, start, end, subject):
+def new_calendar_event(calendar, subject, start, end):
     new_event = calendar.new_event()
     new_event.subject = subject
 
@@ -36,13 +40,28 @@ def new_calendar_event(calendar, start, end, subject):
 
     new_event.save()
 
-class Cube():
-    def __init__(self):
-        self.current_side = "off"
-        self.time_on_current_side = 0
+def extend_calendar_event(calendar, subject, original_start, new_end):
+    query = calendar.new_query('start').greater_equal(original_start)
+    query.new('end').less_equal(new_end)
+    query.chain('and').on_attribute('subject').contains(subject)
 
-        self.__cube_side_map = {
-            1 : "one",
+    # For somereason the example shows the get_event output as an itterable but it doesnt seem to work properly
+    matching_events = [ event for event in calendar.get_events(query = query, include_recurring = False)]
+
+    for event in matching_events:
+        print("Found '%s' starting at %s, extending out to %s" % ( event.attachment_name, original_start.strftime("%H:%M:%S"), new_end.strftime("%H:%M:%S")))
+        event.end = new_end
+        event.save()
+
+class Cube():
+    def __init__(self, timezone):
+        self.current_face = "off"
+        self.start_time_of_face = pytz.timezone(timezone).localize(datetime.now())
+        self.previous_face = "off"
+        self.time_on_current_face = 0
+
+        self.__cube_face_map = {
+            1 : "off",
             2 : "two",
             3 : "three",
             4 : "four",
@@ -50,48 +69,60 @@ class Cube():
             6 : "six",
         }
 
-    def has_face_change(self):
-        # Talk to the ZigBee interface, get the current side
-        print("Previous side was", self.current_side)
-
+    def has_face_changed_in_the_last_x_minutes(self, minutes_elapsed):
         # TODO: Zigbee checks here
-        from random import randint
-        new_side = self.__cube_side_map[randint(1,6)]
+        # from random import randint
+        # self.current_face = self.__cube_face_map[randint(1,6)]
+        self.previous_face = self.current_face
+        self.current_face = self.__cube_face_map[4]
 
-        if new_side == self.current_side:
-            print("Side hasn't changed, it's still", self.current_side)
+        if self.previous_face == self.current_face:
+            # print("Face has not changed, it is still", self.previous_face )
+            self.time_on_current_face += minutes_elapsed
             return False
         else:
-            print("New side is", new_side)
-            self.current_side = new_side
+            # print("Face has changed to", self.current_face)
+            self.time_on_current_face = 0
+            self.start_time_of_face = pytz.timezone(timezone).localize(datetime.now())
             return True
 
 
 if __name__ == '__main__':
-    load_dotenv()
+    tick_length_mins = 5 #15
+    timezone = 'Europe/London'
 
     account = o365_auth()
 
     schedule = account.schedule(resource = os.getenv('CALENDAR_OWNER'))
     calendar = schedule.get_calendar(calendar_name=os.getenv('CALENDAR_NAME'))
 
-    TheCube = Cube()
+    TheCube = Cube(timezone)
 
     while True:
-        print("Checking cube sides")
-        if TheCube.has_face_change():
-            print("Saving new calendar event")
+        print("Checking cube faces...")
 
-            new_calendar_event(
+        if TheCube.has_face_changed_in_the_last_x_minutes(minutes_elapsed = tick_length_mins):
+            print("Cube face has changed to %s, saving calendar event" % (TheCube.current_face))
+
+            if TheCube.current_face.upper() != "OFF":
+                new_calendar_event(
+                    calendar = calendar,
+                    subject = "Focus was on: %s" % (TheCube.current_face),
+                    start = TheCube.start_time_of_face,
+                    end = pytz.timezone(timezone).localize(datetime.now()),
+                )
+
+        else:
+            print("Cube face has not changed, its still %s, extending the event" % (TheCube.current_face))
+
+            extend_calendar_event(
                 calendar = calendar,
-                subject = "Billing for: %s" % (TheCube.current_side),
-                start = datetime.now() - timedelta(minutes = TheCube.time_on_current_side),
-                end = datetime.now(),
+                subject = "Focus was on: %s" % (TheCube.current_face),
+                original_start = TheCube.start_time_of_face - timedelta(minutes = 1),
+                new_end = pytz.timezone(timezone).localize(datetime.now()),
             )
 
-        for i in range(10):
-            print("sleeping", 10 - i, "minutes")
-            time.sleep(60)
+        for i in IncrementalBar("Sleeping for %s minutes" % (tick_length_mins), suffix='%(percent).0f%% (%(eta)ds)').iter(range(tick_length_mins * 60)):
+            time.sleep(1)
 
-
-    print()
+        print()
