4638f69701fa254320d24a047b59e80a24cdf9b1
Aiden Vaines <aiden@vaines.org>
2023-02-17 15:40:06 +0000

    Refactor api container (#1)
    * switching over to a container model
    
    * improving status page
    
    * fixing tick process
    
    * fixing tick process
    
    * fixing 307s
    
    * fixing calendar creation
    
    * fixing calendar creation
    
    * fixing calendar creation
    
    * fix for hisenbug async issue
    
    * fix for hisenbug async issue
    
    * various fixes

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
+
 import os
-import time
 import pytz
 
+from fastapi import FastAPI
 from datetime import datetime, timedelta
 from O365 import Account
-from progress.bar import IncrementalBar
+import asyncio
 
+app = FastAPI()
 
-from dotenv import load_dotenv
-load_dotenv()
+class Cube():
+    def __init__(self, timezone):
+        self.current_face = "off"
+        self.start_time_of_face = pytz.timezone(timezone).localize(datetime.now())
+        self.previous_face = "off"
+        self.time_on_current_face = 0
+
+        self.cube_face_map = {
+            "one": os.getenv('CUBE_FACE_ONE',"1"),
+            "two": os.getenv('CUBE_FACE_TWO',"2"),
+            "three": os.getenv('CUBE_FACE_THREE',"3"),
+            "four": os.getenv('CUBE_FACE_FOUR',"4"),
+            "five": os.getenv('CUBE_FACE_FIVE',"5"),
+            "six": os.getenv('CUBE_FACE_SIX',"6"),
+        }
+
+    def change_face(self, new_face):
+        self.previous_face = self.current_face
+        self.current_face = self.cube_face_map[new_face]
+
+    def tick(self, minutes_elapsed: int):
+        if self.previous_face == self.current_face:
+            # Face has NOT changed since last time we checked
+            self.time_on_current_face += minutes_elapsed
+        else:
+            # Face has changed since last time we checked
+            self.time_on_current_face = 0
+            self.start_time_of_face = pytz.timezone(timezone).localize(datetime.now())
+            self.previous_face = self.current_face
+
 
 def o365_auth():
     credentials = (
@@ -41,78 +71,51 @@ def new_calendar_event(calendar, subject, start, end):
     new_event.save()
 
 def extend_calendar_event(calendar, subject, original_start, new_end):
-    query = calendar.new_query('start').greater_equal(original_start)
+    adjusted_start = original_start - timedelta(minutes = 2) # Make the window slightly wider
+
+    query = calendar.new_query('start').greater_equal(adjusted_start)
     query.new('end').less_equal(new_end)
     query.chain('and').on_attribute('subject').contains(subject)
 
     # For somereason the example shows the get_event output as an itterable but it doesnt seem to work properly
     matching_events = [ event for event in calendar.get_events(query = query, include_recurring = False)]
 
-    for event in matching_events:
+    if len(matching_events) == 1:
+        event = matching_events[0]
         print("Found '%s' starting at %s, extending out to %s" % ( event.attachment_name, original_start.strftime("%H:%M:%S"), new_end.strftime("%H:%M:%S")))
         event.end = new_end
         event.save()
 
-class Cube():
-    def __init__(self, timezone):
-        self.current_face = "off"
-        self.start_time_of_face = pytz.timezone(timezone).localize(datetime.now())
-        self.previous_face = "off"
-        self.time_on_current_face = 0
+    # TODO: this shouldn't need to exist
+    elif len(matching_events) > 1:
+        print(f"Found {len(matching_events)} events' which is odd. attempting to extend them all.")
+        for event in matching_events:
+            print("Extending '%s'; starting at %s, moving end time out to %s" % ( event.attachment_name, original_start.strftime("%H:%M:%S"), new_end.strftime("%H:%M:%S")))
+            event.end = new_end
+            event.save()
 
-        self.cube_face_map = {
-            "one": "Engineering Leadership",
-            "two": "Internal",
-            "three": "Client",
-            "four": "Off",
-            "five": "Recruitment",
-            "six": "Personal",
-        }
+    else:
+        print("Couldn't find a matching event, creating one")
+        new_calendar_event(
+            calendar = calendar,
+            subject = "Focus was on: %s" % (TheCube.current_face),
+            start = original_start,
+            end = new_end,
+        )
 
-    def has_face_changed_in_the_last_x_minutes(self, minutes_elapsed):
-        # TODO: Zigbee checks here
-        # from random import randint
-        # self.current_face = self.__cube_face_map[randint(1,6)]
-        # self.previous_face = self.current_face
-        # self.current_face = self.cube_face_map["four"]
-        path_to_face_file = "./cur_face"
-
-        if os.path.exists(path_to_face_file):
-            self.previous_face = self.current_face
-
-            with open(path_to_face_file) as f:
-                face_from_file = [line.strip() for line in f.readlines()][0]
-
-            self.current_face = self.cube_face_map[face_from_file]
-
-            if self.previous_face == self.current_face:
-                self.time_on_current_face += minutes_elapsed
-                return False
-            else:
-                # print("Face has changed to", self.current_face)
-                self.time_on_current_face = 0
-                self.start_time_of_face = pytz.timezone(timezone).localize(datetime.now())
-                return True
-
-
-if __name__ == '__main__':
-    tick_length_mins = 15
-    timezone = 'Europe/London'
-
-    account = o365_auth()
-
-    schedule = account.schedule(resource = os.getenv('CALENDAR_OWNER'))
-    calendar = schedule.get_calendar(calendar_name=os.getenv('CALENDAR_NAME'))
-
-    TheCube = Cube(timezone)
+async def tick(interval: int = 10):
 
     while True:
-        print("Checking cube faces...")
+        # code to run periodically starts here
+        TheCube.tick(int(interval))
 
-        if TheCube.has_face_changed_in_the_last_x_minutes(minutes_elapsed = tick_length_mins):
-            print("Cube face has changed to %s, saving calendar event" % (TheCube.current_face))
+        print(f"The '{TheCube.current_face}' face has been active for {TheCube.time_on_current_face} minutes, \
+previously it was on the '{TheCube.previous_face}' face, sleeping for {interval} minute(s)")
 
-            if TheCube.current_face != "Off":
+        if TheCube.current_face.upper() != "OFF":
+            if TheCube.current_face != TheCube.previous_face:
+                print(f"New calendar entry for {TheCube.current_face}")
+                # The Cube face has changed
                 new_calendar_event(
                     calendar = calendar,
                     subject = "Focus was on: %s" % (TheCube.current_face),
@@ -120,17 +123,83 @@ if __name__ == '__main__':
                     end = pytz.timezone(timezone).localize(datetime.now()),
                 )
 
-        else:
-            print("Cube face has not changed, its still %s, extending the event" % (TheCube.current_face))
+            else:
+                # The Cube Face has not changed since last tick
+                print(f"Extending calendar entry for {TheCube.current_face}")
+                extend_calendar_event(
+                    calendar = calendar,
+                    subject = "Focus was on: %s" % (TheCube.current_face),
+                    original_start = TheCube.start_time_of_face - timedelta(minutes = int(interval)),
+                    new_end = pytz.timezone(timezone).localize(datetime.now()),
+                )
 
-            extend_calendar_event(
-                calendar = calendar,
-                subject = "Focus was on: %s" % (TheCube.current_face),
-                original_start = TheCube.start_time_of_face - timedelta(minutes = 1),
-                new_end = pytz.timezone(timezone).localize(datetime.now()),
-            )
+        # code to run periodically ends here
+        await asyncio.sleep(int(interval) * 60)
 
-        for i in IncrementalBar("Sleeping for %s minutes" % (tick_length_mins), suffix='%(percent).0f%% (%(eta)ds)').iter(range(tick_length_mins * 60)):
-            time.sleep(1)
+# FastAPI Methods
+@app.get("/")
+async def root():
+    return {
+        "current_face": TheCube.current_face,
+        "face_at_last_interval": TheCube.previous_face,
+        "time_on_face": TheCube.time_on_current_face,
+        "check_interval": tick_interval_minutes,
+        "face_map": TheCube.cube_face_map,
+    }
 
-        print()
+@app.post("/change")
+async def change_face(new_face: str = "one"):
+    TheCube.change_face(new_face)
+
+    return {
+        "new_face": TheCube.current_face,
+        "face_at_last_interval": TheCube.previous_face,
+    }
+
+# Initialise backup process for looping and time management
+@app.on_event("startup")
+async def schedule_periodic():
+    # Fix for Heisenbug async issue: https://textual.textualize.io/blog/2023/02/11/the-heisenbug-lurking-in-your-async-code/
+    background_tasks = set()
+
+    task = asyncio.create_task(tick(tick_interval_minutes))
+    background_tasks.add(task)
+    task.add_done_callback(background_tasks.discard)
+
+
+# Globals
+timezone = 'Europe/London'
+TheCube = Cube(timezone)
+tick_interval_minutes = os.getenv("INTERVAL", 1)
+
+account = o365_auth()
+schedule = account.schedule(resource = os.getenv('CALENDAR_OWNER'))
+calendar = schedule.get_calendar(calendar_name=os.getenv('CALENDAR_NAME'))
+
+
+# Local Execution
+if __name__ == "__main__":
+    import uvicorn
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    required_env_vars = [
+        "CALENDAR_NAME",
+        "CALENDAR_OWNER",
+        "CLIENT_ID",
+        "CLIENT_SECRET",
+        "TENANT_ID",
+    ]
+
+    for required_env_var in required_env_vars:
+        missing_vars = []
+        if os.getenv(required_env_var) is None:
+            print("Environmental variable:", required_env_var, "unset")
+            missing_vars.append(required_env_var)
+
+        if len(missing_vars) > 0:
+            raise RuntimeError("The environmental variables %s are missing" % (", ".join(missing_vars)))
+
+        print("%s set to %s" % (required_env_var, os.environ.get(required_env_var)))
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
